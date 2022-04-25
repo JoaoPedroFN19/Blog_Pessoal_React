5163fa8d0ce923fb35ca3483e993c39f5cfe7882
JoaoPedro <joaopedroflauzinonascimento@gmail.com>
2022-04-25 16:42:31 -0300

    Primeiro Projeto React

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './Home.css';
+
+function Home() {
+    return (
+        <>
+            <h1 className="titulo">Home</h1>
+            <img src="https://i.pinimg.com/736x/3d/74/b1/3d74b17105ce1d2046935a29331cc55e.jpg" alt="imagem de casa" className="img"/>
+        </>
+    );
+}
+
+export default Home;
