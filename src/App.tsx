3b76a7ea44d1ccd6459ccab1a5af296c0b503225
JoaoPedro <joaopedroflauzinonascimento@gmail.com>
2022-04-28 12:21:31 -0300

    Atualizando Projeto - 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,29 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/estaticos/navbar/Navbar';
 import Footer from './components/estaticos/footer/Footer';
-import './App.css';
 import Home from './paginas/home/Home';
+import './App.css';
+import Login from './paginas/login/Login';
 
 
 function App() {
-  return (
-   <>
-      <Navbar />
-      <Home />
-      <Footer />
-   </>
-  );
+    return (
+        <Router>
+            <Navbar />
+            <Routes>
+
+                <Route path="/" element={<Home />} />
+
+                <Route path="/home" element={<Home />} />
+
+                <Route path="/login" element={<Login />} />
+            </Routes>
+            <Footer />
+        </Router>
+
+
+    );
 }
 
 export default App;
