4a281a5445cfb5ab6aff30d84736fb4fc319c69e
JoaoPedro <joaopedroflauzinonascimento@gmail.com>
2022-05-05 17:07:27 -0300

    Atualizando

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,72 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/estaticos/navbar/Navbar';
 import Footer from './components/estaticos/footer/Footer';
-import Home from './paginas/home/Home';
-import './App.css';
-import Login from './paginas/login/Login';
 import CadastroUsuario from './paginas/cadastroUsuario/CadastroUsuario';
+import Home from './paginas/home/Home';
+import Login from './paginas/login/Login';
+import './App.css';
 import ListaTema from './components/temas/listatema/ListaTema';
 import ListaPostagem from './components/postagens/listapostagem/ListaPostagem';
+import CadastroPost from './components/postagens/cadastroPost/CadastroPost';
+import CadastroTema from './components/temas/cadastroTema/CadastroTema';
+import DeletarPostagem from './components/postagens/deletarPostagem/DeletarPostagem';
+import DeletarTema from './components/temas/deletarTema/DeletarTema';
+
 
 function App() {
   return (
     <Router>
       <Navbar />
-      <Routes>
+      <Switch>
+        <div style={{ minHeight: '100vh' }}>
 
-        <Route path="/" element={<Login  />} />
+          <Route exact path='/'>
+            <Login />
+          </Route>
 
-        <Route path="/home" element={<Home />} />
+          <Route path='/login'>
+            <Login />
+          </Route>
 
-        <Route path="/login" element={<Login />} />
+          <Route path='/home'>
+            <Home />
+          </Route>
 
-        <Route path="/cadastrousuario" element={<CadastroUsuario/>} />
+          <Route path='/cadastrousuario'>
+            <CadastroUsuario />
+          </Route>
+          <Route path='/temas'>
+            <ListaTema />
+          </Route>
+          <Route path='/posts'>
+            <ListaPostagem />
+          </Route>
 
-        <Route path="/temas" element={<ListaTema />} />
+          <Route exact path='/formularioPostagem'>
+            <CadastroPost />
+          </Route>
+          <Route exact path='/formularioPostagem/:id'>
+            <CadastroPost />
+          </Route>
+          <Route exact path='/formularioTema'>
+            <CadastroTema />
+          </Route>
+          <Route exact path='/formularioTema/:id'>
+            <CadastroTema />
+          </Route>
+          <Route path='/deletarPostagem/:id'>
+            <DeletarPostagem />
+          </Route>
+          <Route path='/deletarTema/:id'>
+            <DeletarTema />
+          </Route>
 
-        <Route path="/posts" element={<ListaPostagem />} />
 
-
-      </Routes>
+        </div>
+      </Switch>
       <Footer />
     </Router>
-
   );
 }
 
--- src/paginas/login/Login.tsx
+++ src/paginas/login/Login.tsx
@@ -7,7 +7,7 @@ import UserLogin from '../../models/UserLogin';
 import './Login.css';
 
 function Login() {
-    let navigate = useNavigate();
+    let history = useNavigate();
     const [token, setToken] = useLocalStorage('token');
     const [userLogin, setUserLogin] = useState<UserLogin>(
         {
@@ -16,32 +16,32 @@ function Login() {
             senha: '',
             token: ''
         }
-    )
+        )
 
-    function updatedModel(e: ChangeEvent<HTMLInputElement>) {
+        function updatedModel(e: ChangeEvent<HTMLInputElement>) {
 
-        setUserLogin({
-            ...userLogin,
-            [e.target.name]: e.target.value
-        })
-    }
-
-    useEffect(() => {
-        if (token != '') {
-            navigate('/home')
+            setUserLogin({
+                ...userLogin,
+                [e.target.name]: e.target.value
+            })
         }
-    }, [token])
 
-    async function onSubmit(e: ChangeEvent<HTMLFormElement>) {
-        e.preventDefault();
-        try {
-            await login(`/usuarios/logar`, userLogin, setToken)
+            useEffect(()=>{
+                if(token != ''){
+                    history.push('/home')
+                }
+            }, [token])
 
-            alert('Usuário logado com sucesso!');
-        } catch (error) {
-            alert('Dados do usuário inconsistentes. Erro ao logar!');
+        async function onSubmit(e: ChangeEvent<HTMLFormElement>){
+            e.preventDefault();
+            try{
+                await login(`/usuarios/logar`, userLogin, setToken)
+
+                alert('Usuário logado com sucesso!');
+            }catch(error){
+                alert('Dados do usuário inconsistentes. Erro ao logar!');
+            }
         }
-    }
 
     return (
         <Grid container direction='row' justifyContent='center' alignItems='center'>
@@ -49,12 +49,12 @@ function Login() {
                 <Box paddingX={20}>
                     <form onSubmit={onSubmit}>
                         <Typography variant='h3' gutterBottom color='textPrimary' component='h3' align='center' className='textos1'>Entrar</Typography>
-                        <TextField value={userLogin.usuario} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)} id='usuario' label='usuário' variant='outlined' name='usuario' margin='normal' fullWidth />
-                        <TextField value={userLogin.senha} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)} id='senha' label='senha' variant='outlined' name='senha' margin='normal' type='password' fullWidth />
+                        <TextField value={userLogin.usuario} onChange={(e:ChangeEvent<HTMLInputElement>) => updatedModel(e)} id='usuario' label='usuário' variant='outlined' name='usuario' margin='normal' fullWidth />
+                        <TextField value={userLogin.senha} onChange={(e:ChangeEvent<HTMLInputElement>) => updatedModel(e)} id='senha' label='senha' variant='outlined' name='senha' margin='normal' type='password'fullWidth />
                         <Box marginTop={2} textAlign='center'>
-                            <Button type='submit' variant='contained' color='primary'>
-                                Logar
-                            </Button>
+                                <Button type='submit' variant='contained' color='primary'>
+                                    Logar
+                                </Button>
                         </Box>
                     </form>
                     <Box display='flex' justifyContent='center' marginTop={2}>
@@ -64,7 +64,7 @@ function Login() {
                         <Link to='/cadastrousuario'>
                             <Typography variant='subtitle1' gutterBottom align='center' className='textos1'>Cadastre-se</Typography>
                         </Link>
-
+                            
                     </Box>
                 </Box>
             </Grid>
