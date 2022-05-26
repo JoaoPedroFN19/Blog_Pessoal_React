1b68ce9c5e3ed2b93346308adf7c4c6c7a16be5e
JoaoPedro <joaopedroflauzinonascimento@gmail.com>
2022-05-26 10:46:06 -0300

    Atualizando cores

--- src/components/estaticos/footer/Footer.tsx
+++ src/components/estaticos/footer/Footer.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import InstagramIcon from '@material-ui/icons/Instagram';
-import FacebookIcon from '@material-ui/icons/Facebook';
+import InstagramIcon from '@material-ui/icons/GitHub';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import {Typography, Box, Grid } from '@material-ui/core';
 
@@ -9,31 +8,25 @@ function Footer() {
         <>
             <Grid container direction="row" justifyContent="center" alignItems="center">
                 <Grid alignItems="center" item xs={12}>
-                    <Box style={{ backgroundColor: "#3F51B5", height: "120px" }}>
+                    <Box style={{ backgroundColor: "midnightblue", height: "100px" }}>
                         <Box paddingTop={1} display="flex" alignItems="center" justifyContent="center">
-                            <Typography variant="h5" align="center" gutterBottom style={{ color: "white" }}>Siga-nos nas redes sociais </Typography>
+                            <Typography variant="h5" align="center" gutterBottom style={{ color: "White" }}>Siga-me nas redes sociais </Typography>
                         </Box>
                         <Box display="flex" alignItems="center" justifyContent="center">
-                            <a href="https://www.facebook.com/generationbrasil" target="_blank" rel="noopener noreferrer">
-                                <FacebookIcon style={{ fontSize: 60, color: "white" }} />
+                           
+                            <a href="https://github.com/JoaoPedroFN19" target="_blank" rel="noopener noreferrer">
+                                <InstagramIcon style={{ fontSize: 50, color: "White" }} />
                             </a>
-                            <a href="https://www.instagram.com/generationbrasil/" target="_blank" rel="noopener noreferrer">
-                                <InstagramIcon style={{ fontSize: 60, color: "white" }} />
-                            </a>
-                            <a href="https://www.linkedin.com/school/generationbrasil/" target="_blank" rel="noopener noreferrer">
-                                <LinkedInIcon style={{ fontSize: 60, color: "white" }} />
+                            <a href="https://www.linkedin.com/in/joão-pedro-flauzino-nascimento-4a931b232/" target="_blank" rel="noopener noreferrer">
+                                <LinkedInIcon style={{ fontSize: 70, color: "White" }} />
                             </a>
                         </Box>
                     </Box>
-                    <Box style={{ backgroundColor: "#303F9F", height: "60px" }}>
-                        <Box paddingTop={1}>
-                            <Typography variant="subtitle2" align="center" gutterBottom style={{ color: "white" }} >© 2020 Copyright:</Typography>
+                    <Box style={{ backgroundColor: "midnightblue", height: "60px" }}>
+                        <Box paddingTop={4}>
+                            <Typography variant="subtitle2" align="center" gutterBottom style={{ color: "White" }} >© 2022 Copyright</Typography>
                         </Box>
-                        <Box>
-                            <a target="_blank" href="https://brasil.generation.org" rel="noopener noreferrer">
-                                <Typography variant="subtitle2" gutterBottom style={{ color: "white" }} align="center">brasil.generation.org</Typography>
-                            </a>
-                        </Box>
+                        
                     </Box>
                 </Grid>
             </Grid>
